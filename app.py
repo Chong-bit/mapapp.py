c89d719b810fd59cd5a5edbc666340e51e099adf
Chong-bit <123046290+Chong-bit@users.noreply.github.com>
2024-12-29 04:49:53 +0800

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,1 @@
-!pip install pyngrok
-from pyngrok import ngrok
 
-# Start ngrok tunnel
-public_url = ngrok.connect(port='8501') # Default Streamlit port
-print(f"Public URL: {public_url}")
-
-# Run Streamlit app
-!streamlit run app.py &
